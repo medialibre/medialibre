f54d6a75eaac424961502869c0db1b6a6bbda4f9
Nikos Roussos <nikos@roussos.cc>
2016-02-20 02:18:41 +0200

    Add founder theme

---
+++ wp-content/themes/founder/footer.php
@@ -0,0 +1,22 @@
+<?php do_action( 'main_bottom' ); ?>
+</section><!-- .main -->
+
+<footer id="site-footer" class="site-footer" role="contentinfo">
+	<?php do_action( 'footer_top' ); ?>
+	<span>
+        <?php
+        $footer_text = '&copy; 2016 <a href="https://www.hackerspace.gr/" target="_blank">Hackerspace.gr</a> | Το περιεχόμενο διατίθεται με άδεια <a href="http://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Commons Attribution-ShareAlike</a>.';
+        $footer_text = apply_filters( 'ct_founder_footer_text', $footer_text );
+        echo wp_kses_post( $footer_text );
+        ?>
+    </span>
+</footer>
+</div>
+</div><!-- .overflow-container -->
+
+<?php do_action( 'body_bottom' ); ?>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>

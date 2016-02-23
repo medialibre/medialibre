f7210e5bcc76c7abac83416cd73e5f06cd72c9e5
Nikos Roussos <nikos@roussos.cc>
2016-02-23 23:11:51 +0200

    Add footer notice

--- wp-content/themes/founder/footer.php
+++ wp-content/themes/founder/footer.php
@@ -5,8 +5,8 @@
 	<?php do_action( 'footer_top' ); ?>
 	<span>
         <?php
-        //$footer_text = '<span class="fa fa-cc"></span> 2016 <a href="https://www.hackerspace.gr/" target="_blank">Hackerspace.gr</a> | Το περιεχόμενο διατίθεται με άδεια <a href="http://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Commons Attribution-ShareAlike</a>.';*/
-        $footer_text = '';
+        //$footer_text = '<span class="fa fa-cc"></span> 2016 <a href="https://www.hackerspace.gr/" target="_blank">Hackerspace.gr</a> | Το περιεχόμενο διατίθεται με άδεια <a href="http://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Commons Attribution-ShareAlike</a>.';
+		$footer_text = 'Το περιεχόμενο διατίθεται με άδεια <a href="http://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Commons Attribution-ShareAlike</a>.';
         $footer_text = apply_filters( 'ct_founder_footer_text', $footer_text );
         echo wp_kses_post( $footer_text );
         ?>

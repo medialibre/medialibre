67548b236c8ba36ebc6919bdff46fe9baa0e19e0
Nikos Roussos <comzeradd@fsfe.org>
2016-10-21 00:36:02 +0300

    Adjust tags widget
    * Add tags word on toggle
    * Hide widget title for tags
    * Wide tags container

--- wp-content/themes/founder/sidebar-primary.php
+++ wp-content/themes/founder/sidebar-primary.php
@@ -6,7 +6,7 @@
 	<aside class="sidebar sidebar-primary" id="sidebar-primary" role="complementary">
 		<button id="toggle-sidebar" class="toggle-sidebar" name="toggle-sidebar" aria-expanded="false">
 			<span class="screen-reader-text"><?php _e( 'open sidebar', 'founder' ); ?></span>
-			<i class="fa fa-angle-down" title="<?php _e( 'sidebar icon', 'founder' ); ?>" aria-hidden="true"></i>
+            tags <i class="fa fa-angle-down" title="<?php _e( 'sidebar icon', 'founder' ); ?>" aria-hidden="true"></i>
 		</button>
 		<div id="sidebar-primary-content" class="sidebar-primary-content">
 			<div id="sidebar-primary-widgets" class="sidebar-primary-widgets active-<?php echo $widget_count; ?>">

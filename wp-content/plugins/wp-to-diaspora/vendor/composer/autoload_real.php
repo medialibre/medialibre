5c1112345391afc245792a92e77622544a746fbe
Nikos Roussos <comzeradd@fsfe.org>
2017-01-17 12:59:27 +0200

    wp-to-diaspora

--- wp-content/plugins/wp-to-diaspora/vendor/composer/autoload_real.php
+++ wp-content/plugins/wp-to-diaspora/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit605513f3d5d4ba5250a5f21744f73d4f
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit605513f3d5d4ba5250a5f21744f73d4f', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 

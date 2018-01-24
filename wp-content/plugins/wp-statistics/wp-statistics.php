5c64cd18ffdeac88a08ae9f6bd18c3196b8bc334
Nikos Roussos <nikos@roussos.cc>
2018-01-24 14:41:16 +0200

    Update wp-statistics

--- wp-content/plugins/wp-statistics/wp-statistics.php
+++ wp-content/plugins/wp-statistics/wp-statistics.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Statistics
  * Plugin URI: https://wp-statistics.com/
  * Description: Complete WordPress Analytics and Statistics for your site!
- * Version: 12.3.1
+ * Version: 12.3.5
  * Author: Verona Labs
  * Author URI: http://veronalabs.com/
  *
@@ -11,6 +11,8 @@
  * Domain Path: /languages/
  */
 
-define('WP_STATISTICS_MAIN_FILE', __FILE__);
-require plugin_dir_path(__FILE__) . 'includes/classes/class-wp-statistics.php';
-(new WP_Statistics)->run();
+define( 'WP_STATISTICS_MAIN_FILE', __FILE__ );
+require plugin_dir_path( __FILE__ ) . 'includes/classes/class-wp-statistics.php';
+
+$WP_Statistics = new WP_Statistics;
+$WP_Statistics->run();

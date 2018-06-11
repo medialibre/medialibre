8dd7e0540953c7db94e372f1979a73bc7ae21e43
Nikos Roussos <nikos@roussos.cc>
2018-06-11 15:47:32 +0300

    Update footer notice

--- README.md
+++ README.md
@@ -4,6 +4,4 @@ This is the source code of [medialibre.net](https://medialibre.net).
 
 ## License
 
-&copy; 2016 [Hackerspace.gr](https://www.hackerspace.gr).
-
 Licensed under the [AGPLv3](LICENSE).
--- wp-content/themes/founder/footer.php
+++ wp-content/themes/founder/footer.php
@@ -5,7 +5,7 @@
 	<?php do_action( 'footer_top' ); ?>
 	<span>
         <?php
-		$footer_text = 'Το περιεχόμενο των κειμένων ανήκει στ@ν αρχικ@ συγγραφέα/μέσο, όπου αναγράφεται.<br>Οι μεταφράσεις μας διατίθονται με άδεια <a href="http://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Commons Attribution-ShareAlike</a>.<br><i class="fa fa-power-off"></i> powered by <a href="https://www.hackerspace.gr/" target="_blank">Hackerspace.gr</a>';
+		$footer_text = 'Το περιεχόμενο των κειμένων ανήκει στ@ν αρχικ@ συγγραφέα/μέσο, όπου αναγράφεται.<br>Οι μεταφράσεις μας διατίθονται με άδεια <a href="http://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Commons Attribution-ShareAlike</a>.<br><i class="fa fa-power-off"></i> hosted by <a href="https://libreops.cc/" target="_blank">LibreOps</a>';
         $footer_text = apply_filters( 'ct_founder_footer_text', $footer_text );
         echo wp_kses_post( $footer_text );
         ?>
